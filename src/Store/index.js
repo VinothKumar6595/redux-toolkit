c3e2291ffd9d31deb133289bfa8db687bd288c11
VinothKumar6595 <vinothcassillas@gmail.com>
2023-10-04 20:03:20 +0530

    Redux Toolkit

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,21 +1,67 @@
+import React from "react";
 import { createStore } from "redux";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-const counterReducer = (state = { counter: 0 }, action) => {
-  if (action.type === "incrementBy2") {
-    return { counter: state.counter + 2 };
-  }
-  if (action.type === "decrementBy2") {
-    return { counter: state.counter - 2 };
-  }
-  if (action.type === "incrementBy5") {
-    return { counter: state.counter + 5 };
-  }
-  if (action.type === "decrementBy5") {
-    return { counter: state.counter - 5 };
-  }
-  return state;
-};
+const initialCounterState = { counter: 0, showCounter: true };
 
-const store = createStore(counterReducer);
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: initialCounterState,
+  reducers: {
+    incrementBy2(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    decrementBy2(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    incrementBy5(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    deccrementBy5(state, action) {
+      state.counter = state.counter - action.payload;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
+const initialAuthState = { isAuthenticated: false };
+const authSlice = createSlice({
+  name: "authentication",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
+// const counterReducer = (state = { counter: 0 }, action) => {
+//   if (action.type === "incrementBy2") {
+//     return { counter: state.counter + action.value };
+//   }
+//   if (action.type === "decrementBy2") {
+//     return { counter: state.counter - action.value };
+//   }
+//   if (action.type === "incrementBy5") {
+//     return { counter: state.counter + action.value };
+//   }
+//   if (action.type === "decrementBy5") {
+//     return { counter: state.counter - action.value };
+//   }
+//   return state;
+// };
+
+counterSlice.actions.decrementBy2();
+
+const store = configureStore({
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
+});
+
+export const CounterActions = counterSlice.actions;
+export const AuthActions = authSlice.actions;
 export default store;
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,37 +1,44 @@
 import classes from "./Counter.module.css";
 import { useSelector, useDispatch } from "react-redux";
+import { CounterActions } from "../Store";
 
 const Counter = () => {
-  const counter = useSelector((state) => state.counter);
+  const counter = useSelector((state) => state.counter.counter);
+  const show = useSelector((state) => state.counter.showCounter);
+
   const dispatch = useDispatch();
   const toggleCounterHandler = () => {};
 
   const incrementHandler = () => {
-    dispatch({ type: "incrementBy2" });
+    dispatch(CounterActions.incrementBy2(2));
   };
   const decrementHandler = () => {
-    dispatch({ type: "decrementBy2" });
+    dispatch(CounterActions.decrementBy2(2));
   };
 
   const incrementBy5Handler = () => {
-    dispatch({ type: "incrementBy5" });
+    dispatch(CounterActions.incrementBy5(5));
   };
 
   const decrementBy5Handler = () => {
-    dispatch({ type: "decrementBy5" });
+    dispatch(CounterActions.deccrementBy5(5));
+  };
+
+  const toggleCounter = () => {
+    dispatch(CounterActions.toggleCounter());
   };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Plus 2</button>
         <button onClick={decrementHandler}>Minus 2</button>
         <button onClick={incrementBy5Handler}>Plus 5</button>
         <button onClick={decrementBy5Handler}>Minus 5</button>
       </div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+      <button onClick={toggleCounter}>Toggle Counter</button>
     </main>
   );
 };
